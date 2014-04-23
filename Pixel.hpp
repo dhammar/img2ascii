78b21d1d449ca77246ef979e44cb507f8d386bfe
Dave Hammarstedt <ddh@iastate.edu>
2014-04-23 11:43:26 -0500

    Fixed file mismatch in Pixel.hpp

--- Pixel.hpp
+++ Pixel.hpp
@@ -1,12 +1,28 @@
-#include "Pixel.cpp"
-#include <vector>
+#ifndef PIXEL_HPP_
+#define PIXEL_HPP_
 
-class ImageLoader
+class Pixel
 {
+	private:
+		unsigned char r;
+		unsigned char g;
+		unsigned char b;
+		unsigned char alpha;
 
 	public:
-		ImageLoader();
-		~ImageLoader();
-		std::vector< vector<Pixel> > loadImage(FILE* f);
+		Pixel();
+		Pixel(unsigned char r, unsigned char g, unsigned char b, unsigned char x);
+		~Pixel();
+		
+		unsigned char getR(); 
+		unsigned char getG();
+		unsigned char getB();
+		unsigned char getX();
 
-}
+		int setR(unsigned char val);
+		int setG(unsigned char val);
+		int setB(unsigned char val);
+		int setX(unsigned char val);
+
+};   
+#endif
