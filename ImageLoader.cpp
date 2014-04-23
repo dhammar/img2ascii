132da0c9ede915e8d3c5174351e398fc463b0bfc
Dave Hammarstedt <ddh@iastate.edu>
2014-04-23 00:36:19 -0500

    Create ImageLoader.cpp
    Object that loads a given SIMP image file into a 2d vector array of Pixel objects.

---
+++ ImageLoader.cpp
@@ -0,0 +1,16 @@
+
+#include "ImageLoader.hpp"
+#include <vector>
+#include <queue>
+
+ImageLoader::ImageLoader() {}
+ImageLoader::~ImageLoader() {}
+
+
+/*
+* Loads image data from file F and stores it in a 2d vector array containing pixel objects
+*/
+std::vector< std::vector<Pixel> > loadImage(FILE* f)
+{
+	return NULL;
+}
